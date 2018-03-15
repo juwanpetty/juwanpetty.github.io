c0aebcd6b27c8e4f92a9596ffadd7e639b89507d
Juwan Petty <jchpetty@gmail.com>
2018-03-15 00:14:50 -0400

    Clean up CSS and JS files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 feather.replace();
+
+let menuIcon = document.querySelector('.js-menu-icon');
+let menu = document.querySelector('.js-menu');
+
+function toggleMenu() {
+    menu.classList.toggle('');
+}
+
+menuIcon.addEventListener('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+});
